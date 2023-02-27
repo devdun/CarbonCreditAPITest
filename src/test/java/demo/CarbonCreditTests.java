5ba099838a051147d12b18675db89a42c82098d2
Devdun <devdun90@gmail.com>
2023-02-27 23:43:05 +0530

    initial commit

---
+++ src/test/java/demo/CarbonCreditTests.java
@@ -0,0 +1,74 @@
+package demo;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.response.ResponseBody;
+import io.restassured.specification.RequestSpecification;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+public class CarbonCreditTests {
+
+	@Test
+	public void validateCarbonCredits() {
+		RestAssured.baseURI = "https://api.tmsandbox.co.nz/v1";
+		RequestSpecification request = given();
+
+		// Send a GET request to the API endpoint
+		Response response = request.get("/Categories/6327/Details.json?catalogue=false");
+
+		// Validate the response code
+		response.then().statusCode(200);
+
+		// Validate the "Name" field is equal to "Carbon credits"
+		response.then().assertThat().body("Name", equalTo("Carbon credits"));
+
+		// Validate the "CanRelist" field is true
+		response.then().assertThat().body("CanRelist", is(true));
+
+		// Validate the "Promotions" element with "Name" equal to "Gallery" has a "Description" containing the text "Good position in category"
+		response.then().assertThat().body("Promotions.find { it.Name == 'Gallery' }.Description", containsString("Good position in category"));
+
+		// Additional tests for error handling
+		response.then()
+				.assertThat().contentType("application/json")
+				.and().header("Content-Length", "1398")
+//				.and().header("Content-Encoding", "")
+				.and().time(lessThan(5000L)); // Ensure the response time is less than 5 seconds
+	}
+
+	@Test
+	public void validateInvalidEndpoint() {
+		RestAssured.baseURI = "https://api.tmsandbox.co.nz/v1";
+		RequestSpecification request = given();
+
+		// Send a GET request to an invalid API endpoint
+		Response response = request.get("/invalid-endpoint");
+
+		// Validate the response code is a 404 Not Found
+		response.then().statusCode(404);
+
+		// Validate the error message in the response body
+		ResponseBody body = response.getBody();
+		String bodyAsString = body.asString();
+		Assert.assertEquals(bodyAsString.contains("File or directory not found."),true,"404 correct validation message");
+	}
+
+	@Test
+	public void validateInvalidMethod() {
+		RestAssured.baseURI = "https://api.tmsandbox.co.nz/v1";
+		RequestSpecification request = given();
+
+		// Send a POST request to the API endpoint
+		Response response = request.post("/Categories/6327/Details.json?catalogue=false");
+
+		// Validate the response code is a 405 Method Not Allowed
+		response.then().statusCode(405);
+
+		// Validate the error message in the response body
+		response.then().assertThat().body("error", equalTo("Method Not Allowed"));
+	}
+}
